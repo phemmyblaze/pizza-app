e11a0ea42d3ee53fcbe8d00f1fb6b2e0ee6915dd
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-09-07 01:41:49 +0100

    1:41am

--- src/index.js
+++ src/index.js
@@ -74,11 +74,14 @@ function Menu() {
 			<h2>Our Menu</h2>
 
 			{numPizzas > 0 ? (
-				<ul className="pizzas">
-					{pizzas.map((pizza) => (
-						<Pizza pizzaObj={pizza} key={pizza.name} />
-					))}
-				</ul>
+				<>
+					<p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+					<ul className="pizzas">
+						{pizzas.map((pizza) => (
+							<Pizza pizzaObj={pizza} key={pizza.name} />
+						))}
+					</ul>
+				</>
 			) : (
 				<p>We're still working on our menu. Please come back later</p>
 			)}
@@ -87,14 +90,13 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-	if (pizzaObj.soldOut) return null;
 	return (
-		<li className="pizza">
+		<li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
 			<img src={pizzaObj.photoName} alt={pizzaObj.name} />
 			<div className="">
 				<h3>{pizzaObj.name}</h3>
 				<p>{pizzaObj.ingredients}</p>
-				<span>{pizzaObj.price}</span>
+				<span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
 			</div>
 		</li>
 	);
@@ -120,10 +122,12 @@ function Footer() {
 	);
 }
 
-function Order({ closeHour }) {
+function Order({ closeHour, openHour }) {
 	return (
 		<div className="order">
-			<p>We're open until {closeHour}:00. Come visit us or order Online </p>
+			<p>
+				We're open from {openHour}:00 until {closeHour}:00. Come visit us or order Online{" "}
+			</p>
 
 			<button className="btn">Order</button>
 		</div>
